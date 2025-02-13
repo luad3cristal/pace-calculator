ed9446b68b19f92f3fc5ffe540f3a5127362f3f7
Luana Lima <luanalima.2006@hotmail.com>
2025-02-12 22:03:24 -0300

    feat: add function to calculate speed and refactor functions

--- src/components/painel/Painel.jsx
+++ src/components/painel/Painel.jsx
@@ -4,29 +4,56 @@ import lightIcon from "../../assets/light-icon.svg";
 import S from "./Painel.module.css";
 
 const Painel = ({ toggleTheme, lightTheme, time, distance, distanceKind }) => {
-  const [pace, setPace] = useState("00:00");
+  const [pace, setPace] = useState();
+  const [speed, setSpeed] = useState("00:00");
 
-  const paceCalc = () => {
-    if (!time || !distance) return;
+  const paceCalc = (distancia, minutagem) => {
+    const paceMin = Math.floor(minutagem / distancia);
+    const paceSec = Math.round(((minutagem / distancia) % 1) * 60);
+
+    setPace(
+      `${paceMin.toString().padStart(2, "0")}:${paceSec
+        .toString()
+        .padStart(2, "0")}`
+    );
+  };
+
+  const speedCalc = (distancia, minutagem) => {
+    const velocidade = (distancia / minutagem) * 60;
+    const speedMin = Math.floor(velocidade);
+    const speedSec = Math.round((velocidade % 1) * 60);
+
+    setSpeed(
+      `${speedMin.toString().padStart(2, "0")}:${speedSec
+        .toString()
+        .padStart(2, "0")}`
+    );
+  };
+
+  const calculoDistancia = () => {
+    //consertar o caso do cálculo de metro
+    
+    if (!time || !distance) {
+      setPace("00:00");
+      setSpeed("00:00");
+      return;
+    }
 
     const minTotal =
-      (time.hr || 0) * 60 + (time.min || 0) + (time.segs || 0) / 60;
+      (time?.hr || 0) * 60 + (time?.min || 0) + (time?.segs || 0) / 60;
     const distancia = distanceKind === "km" ? distance : distance / 1000;
 
-    if (distancia > 0) {
-      const paceMin = Math.floor(minTotal / distancia);
-      const paceSec = Math.round(((minTotal / distancia) % 1) * 60);
-      if (paceMin < 10)
-        setPace(`0${paceMin}:${paceSec.toString().padStart(2, "0")}`);
-      else 
-        setPace(`${paceMin}:${paceSec.toString().padStart(2, "0")}`);
+    if (distancia != 0 && minTotal != 0) {
+      paceCalc(distancia, minTotal);
+      speedCalc(distancia, minTotal);
     } else {
       setPace("00:00");
+      setSpeed("00:00");
     }
   };
 
   useEffect(() => {
-    paceCalc();
+    calculoDistancia();
   }, [time, distance, distanceKind]);
 
   return (
@@ -44,7 +71,7 @@ const Painel = ({ toggleTheme, lightTheme, time, distance, distanceKind }) => {
       <div className={S.speed_box}>
         <h3>Velocidade</h3>
         <div>
-          <h2>00:00</h2>
+          <h2>{speed}</h2>
           <span>km/h</span>
         </div>
       </div>
